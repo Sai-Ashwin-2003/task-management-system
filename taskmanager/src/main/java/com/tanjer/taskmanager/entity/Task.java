20b1a1cf9935a583e36a9936afb194b7a58e6043
Sai-Ashwin-2003 <saiashwin082@gmail.com>
2026-05-22 23:43:31 +0530

    Postgres Database Connected

---
+++ taskmanager/src/main/java/com/tanjer/taskmanager/entity/Task.java
@@ -0,0 +1,25 @@
+package com.tanjer.taskmanager.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private String status;
+
+    private String priority;
+}
